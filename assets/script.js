4b693bafdb393705dc0cfef28dfd43d9496b4390
aldrin <aldrinespinosa1916@gmail.com>
2020-01-10 22:52:22 +0800

    Tootsy

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.skill-slider').slick({
+        dots: false,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll:1,
+        autoplay:true,
+        autoplaySpeed:1500,
+        arrows:false
+        // centerMode: true,
+        // variableWidth: true
+    });
+});
