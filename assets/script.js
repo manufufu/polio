3f8489362846dd896c8d5e052c11ebec6cff2e37
aldrin <aldrinespinosa1916@gmail.com>
2020-01-12 18:23:45 +0800

    Tootsy

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
+$(document).ready(function(){
 
     let btnCnt = document.querySelector(".filter-btns");
     let filterThis = [...document.querySelectorAll(".project-flex")];
-    let smoothScroll = [...document.querySelectorAll(".header-menu-li a")];
 
     $('.skill-slider').slick({
         dots: false,
@@ -18,32 +18,31 @@
 
         if (e.target.classList.contains("btn")) {
             let filter = e.target.dataset.filter;
-
-            function animationThing() {
-                filterThis.map(item => {
-                    item.classList.remove("display-none");
-                    item.classList.add("project-flex--fade");
-                    setTimeout(function () {
-                        item.classList.remove("project-flex--fade");
-                    }, 400);
-                });
-            }
-
-            function animationRemove() {
-                filterThis.filter(item => item.dataset.filterName !== filter).map(item => {
-                    setTimeout(function () {
-                        item.classList.add("display-none");
-                    }, 400);
-                });
-            }
-
             if (filter === "*") {
                 animationThing();
             } else {
                 animationThing();
-                animationRemove();
+                animationRemove(filter);
             }
         }
 
     });
 
+    function animationThing() {
+        filterThis.map(item => {
+            item.classList.remove("display-none");
+            item.classList.add("project-flex--fade");
+            setTimeout(function () {
+                item.classList.remove("project-flex--fade");
+            }, 400);
+        });
+    }
+
+    function animationRemove(filter) {
+        filterThis.filter(item => item.dataset.filterName !== filter).map(item => {
+            setTimeout(function () {
+                item.classList.add("display-none");
+            }, 400);
+        });
+    }
+})
